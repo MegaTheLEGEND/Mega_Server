a506b000bfd22b32ab4825b07ea67fdde4af4ac3
Sean <83432253+MegaTheLEGEND@users.noreply.github.com>
2024-05-18 23:54:11 -0700

    update to newest version
    update to the newest plugins and servers and stuff

--- bungee_v2/start.cmd
+++ bungee_v2/start.cmd
@@ -1,4 +1,4 @@
 title bungee
 @echo off
-java -Xmx128M -Xms128M -jar bungee.jar
+java -Xmx128M -Xms128M -jar waterfall-1.20.jar
 pause
--- paper_server/run.bat
+++ paper_server/run.bat
@@ -1,4 +1,4 @@
 title MC SERVER
 @echo off
-java -Xmx4256M -Xms4256M -jar paper-1.20.1-108.jar
+java -Xmx4256M -Xms4256M -jar paper-server.jar
 pause
